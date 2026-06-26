bdb7e12fed8dfaa2d57583acab44d25e2436f9c4
mzx <voidwind@qq.com>
2026-06-26 18:41:52 +0800

    ai

--- src/agent/mod.rs
+++ src/agent/mod.rs
@@ -1,2 +1,133 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
 /// # Ollama后段Api
 pub mod ollama;
+
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Clan {
+    pub reason: Option<String>,
+    pub message: Option<String>,
+    pub detail: Option<Value>,
+    pub r#type: Option<String>,
+    pub tag: Option<String>,            // 标签
+    pub name: Option<String>,           // 名称
+    pub description: Option<String>,    // 公告
+    pub location: Option<ClanLocation>, // 位置
+    // pub is_family_friendly: Option<bool>,
+    // pub badge_urls: Option<ClanIconUrls>,
+    pub clan_level: Option<i64>, // 等级
+    // pub clan_points: Option<i64>, // 积分
+    // pub clan_builder_base_points: Option<i64>,
+    // pub clan_capital_points: Option<i64>,
+    pub capital_league: Option<ClanInfo>,
+    pub required_trophies: Option<i64>,
+    pub war_frequency: Option<String>, // 部落战频率
+    pub war_win_streak: Option<i64>,   // 连胜
+    pub war_wins: Option<i64>,
+    pub war_ties: Option<i64>,
+    pub war_losses: Option<i64>,
+    // pub is_war_log_public: Option<bool>,
+    pub war_league: Option<ClanInfo>,
+    pub members: Option<i64>,
+    pub member_list: Option<ClanMemberLists>,
+    // pub labels: Option<ClanInfoIcons>,
+    // pub required_builder_base_trophies: Option<i64>,
+    // #[serde(rename = "requiredTownhallLevel")]
+    // pub required_town_hall_level: Option<i64>, // 加入所需大本等级
+    pub clan_capital: Option<ClanCapital>, // 都城
+}
+
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ClanLocation {
+    pub id: Option<i64>,
+    pub name: Option<String>,
+    pub is_country: Option<bool>,
+    pub country_code: Option<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
+pub struct ClanIconUrls {
+    pub small: Option<String>,
+    pub large: Option<String>,
+    pub medium: Option<String>,
+    pub tiny: Option<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
+pub struct ClanInfo {
+    pub id: Option<i64>,
+    pub name: Option<String>,
+}
+
+pub type ClanMemberLists = Vec<ClanMemberList>;
+
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
+#[serde(rename_all(deserialize = "camelCase"))]
+pub struct ClanMemberList {
+    pub tag: Option<String>,
+    pub name: Option<String>,
+    pub role: Option<String>,
+    pub town_hall_level: Option<i64>,
+    // pub exp_level: Option<i64>,
+    pub league: Option<ClanInfo>,
+    // pub trophies: Option<i64>,
+    // pub builder_base_trophies: Option<i64>,
+    // pub clan_rank: Option<i64>,
+    // pub previous_clan_rank: Option<i64>,
+    // pub donations: Option<i64>,
+    // pub donations_received: Option<i64>,
+    // pub player_house: Option<ClanMemberListPlayerHouse>,
+    // pub builder_base_league: Option<ClanInfo>,
+    // pub chat_language: Option<ClanChatLanguage>,
+}
+
+pub type ClanInfoIcons = Vec<ClanInfoIcon>;
+
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ClanInfoIcon {
+    pub id: Option<i64>,
+    pub name: Option<String>,
+    pub icon_urls: Option<ClanIconUrls>,
+}
+
+pub type ClanMemberListPlayerHouseElements = Vec<ClanMemberListPlayerHouseElement>;
+
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
+pub struct ClanMemberListPlayerHouse {
+    pub elements: Option<ClanMemberListPlayerHouseElements>,
+}
+
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
+pub struct ClanMemberListPlayerHouseElement {
+    pub r#type: Option<String>,
+    pub id: Option<i64>,
+}
+
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ClanCapital {
+    pub capital_hall_level: Option<i64>,
+    pub districts: Option<ClanCapitalDistricts>,
+}
+
+pub type ClanCapitalDistricts = Vec<ClanCapitalDistrict>;
+
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ClanCapitalDistrict {
+    pub id: Option<i64>,
+    pub name: Option<String>,
+    pub district_hall_level: Option<i64>,
+}
+
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ClanChatLanguage {
+    pub id: Option<i64>,
+    pub name: Option<String>,
+    pub language_code: Option<String>,
+}
--- src/agent/ollama.rs
+++ src/agent/ollama.rs
@@ -9,14 +9,14 @@ use serde::{Deserialize, Serialize};
 use tokio::io::AsyncReadExt;
 use void_log::log_info;
 
-use crate::app::Config;
+use crate::{agent::Clan, app::Config};
 
 #[rig_derive::rig_tool(
     description = "通过Api获取标签的部落信息，标签以数字和字母组成，通常为3至10位数"
 )]
-async fn get_tag_info(tag: String) -> Result<serde_json::Value, ToolError> {
+async fn get_tag_info(tag: String) -> Result<Clan, ToolError> {
     let api = Config::get().await.get_api();
-    println!("[Tool Call] 查询标签 {} 的数据", &tag);
+    log_info!("[Tool Call] 查询标签 {} 的数据", &tag);
     let tag = tag.trim_start_matches('#');
     let url = format!("https://api.clashofclans.com/v1/clans/%23{}", tag);
     let token = format!("Bearer {}", api.token.unwrap());
@@ -26,10 +26,8 @@ async fn get_tag_info(tag: String) -> Result<serde_json::Value, ToolError> {
         .send()
         .await
         .unwrap();
-    let result = response
-        .json::<serde_json::Value>()
-        .await
-        .unwrap_or_default();
+    let result = response.json::<Clan>().await.unwrap_or_default();
+    log_info!("{:?}", &result);
     Ok(result)
 }
 
@@ -43,25 +41,40 @@ pub async fn agent_run(
     mut messages: Vec<Message>,
 ) -> Result<String, rig::completion::PromptError> {
     log_info!("调用智能体");
+
+    let preamble = Preamble::read().await.unwrap_or_default();
     let config_agent = Config::get().await.get_agent();
     let client = Client::builder()
         .api_key(Nothing)
         .base_url("http://localhost:11434")
         .build()
         .unwrap();
-    let agent = client
-        .agent(config_agent.model.unwrap_or("qwen3:0.6b".to_string()))
-        .preamble(&Preamble::read().await.unwrap_or_default().0)
-        .append_preamble("未指明语言时的一切回答必须为中文")
-        .tools(boxed_tools())
-        .max_tokens(4096)
-        .build();
+    let mut agent = client.agent(config_agent.model.unwrap_or("qwen3:0.6b".to_string()));
+    if let Some(p) = preamble.first_preamble {
+        agent = agent.preamble(&p);
+    } else {
+        agent = agent.preamble("你现在是一个负责查询和整理游戏《部落冲突》的数据助理，查询到的数据都是json格式，字段是英文的，当我以其他语言问你字段信息的时候，你需要将统计结果以询问的语言展示出来");
+    };
+
+    if let Some(ps) = preamble.preambles {
+        for preamble in ps {
+            agent = agent.append_preamble(&preamble);
+        }
+    } else {
+        agent = agent
+            .append_preamble("术语注解：Town Hall是主世界玩法；town_hall_level是大本营等级，又称大本等级；donations指的与捐赠部队有关的信息，玩家圈子俗称捐兵与收兵。对照表不要出现在结果里面")
+            .append_preamble("数据整理需要包括部落名称，部落标签，部落首领标签和名称，部落位置及一些基础信息，大本营等级分布统计，联赛段位分布统计，不要展示非首领成员的信息，只关注部落情况")
+    }
+    let agent = agent.tool(GetTagInfo).max_tokens(20480).build();
 
     agent.chat(prompt, &mut messages).await
 }
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
-struct Preamble(String);
+struct Preamble {
+    first_preamble: Option<String>,
+    preambles: Option<Vec<String>>,
+}
 
 impl Preamble {
     async fn read() -> Option<Self> {
@@ -83,7 +96,8 @@ impl Preamble {
 }
 
 #[tokio::test]
-async fn test() {
-    let a = Preamble::read().await.unwrap_or_default().0;
-    println!("{a}")
+async fn test2() {
+    let messages = Vec::new();
+    let a = agent_run("查询标签#q82u2qr9", messages).await.unwrap();
+    log_info!("{a}")
 }
